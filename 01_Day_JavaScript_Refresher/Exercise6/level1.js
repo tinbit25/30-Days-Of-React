7b972a6f2d51cc739f7a6335a24601109ce22704
tinbit25 <tinbitelias25@gmail.com>
2025-01-03 03:02:22 +0300

    finish ex-5

---
+++ 01_Day_JavaScript_Refresher/Exercise6/level1.js
@@ -0,0 +1,43 @@
+// Exercises
+// const products = [
+//   { product: 'banana', price: 3 },
+//   { product: 'mango', price: 6 },
+//   { product: 'potato', price: ' ' },
+//   { product: 'avocado', price: 8 },
+//   { product: 'coffee', price: 10 },
+//   { product: 'tea', price: '' },
+// ]
+// Print the price of each product using forEach
+
+// Print the product items as follows using forEach
+
+// The price of banana is 3 euros.
+// The price of mango is 6 euros.
+// The price of potato is unknown.
+// The price of avocado is 8 euros.
+// The price of coffee is 10 euros.
+// The price of tea is unknown.
+// Calculate the sum of all the prices using forEach
+
+// Create an array of prices using map and store it in a variable prices
+
+// Filter products with prices
+
+// Use method chaining to get the sum of the prices(map, filter, reduce)
+
+// Calculate the sum of all the prices using reduce only
+
+// Find the first product which doesn't have a price value
+
+// Find the index of the first product which does not have price value
+
+// Check if some products do not have a price value
+
+// Check if all the products have price value
+
+// Explain the difference between forEach, map, filter and reduce
+
+// Explain the difference between filter, find and findIndex
+
+// Explain the difference between some and every
+
