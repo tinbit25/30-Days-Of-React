d2a29b7a793cfa2beebcb48d6ab519329be4d611
tinbit25 <tinbitelias25@gmail.com>
2025-01-05 01:45:53 +0300

    start level1

--- 01_Day_JavaScript_Refresher/Exercise7/level1.js
+++ 01_Day_JavaScript_Refresher/Exercise7/level1.js
@@ -2,18 +2,3 @@
 // Create an Animal class. The class will have name, age, color, legs properties and create different methods
 // Create a Dog and Cat child class from the Animal Class.
 
-class Person {
-    constructor(firstName, lastName) {
-      console.log(this) // Check the output from here
-      this.firstName = firstName
-      this.lastName = lastName
-    }
-  }
-  
-  const person1 = new Person('Asabeneh', 'Yetayeh')
-  const person2 = new Person('Lidiya', 'Tekle')
-  const person3 = new Person('Abraham', 'Yetayeh')
-  
-  console.log(person1)
-  console.log(person2)
-  console.log(person3)
