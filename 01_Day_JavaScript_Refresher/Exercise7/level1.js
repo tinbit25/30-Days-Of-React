6eaf0383c517d1fe90ab9b403c964fcb120ffc45
tinbit25 <tinbitelias25@gmail.com>
2025-01-05 01:49:41 +0300

    finish level1

--- 01_Day_JavaScript_Refresher/Exercise7/level1.js
+++ 01_Day_JavaScript_Refresher/Exercise7/level1.js
@@ -2,3 +2,33 @@
 // Create an Animal class. The class will have name, age, color, legs properties and create different methods
 // Create a Dog and Cat child class from the Animal Class.
 
+class Animal {
+    constructor(name, age, color, legs) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.legs = legs;
+    }
+    getAnimalProperties() {
+        return `Name: ${this.name}, Age: ${this.age}, Color: ${this.color}, Legs: ${this.legs}`;
+    }
+}
+// Create a Dog class that extends the Animal class
+class Dog extends Animal{
+    constructor(name,age,color,legs){
+        super(name,age,color,legs)}
+
+    dogSound(){
+        return 'Woof Woof';
+    }
+}
+class Cat extends Animal{
+    constructor(name,age,color,legs){
+        super(name,age,color,legs)}
+
+    catSound(){
+        return 'Meow Meow';
+    }
+}
+const dog = new Dog('Doggy', 2, 'Brown', 4);
+console.log(dog.getAnimalProperties());
