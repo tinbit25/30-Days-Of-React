4762f7f10e38e204e60742b35abaf4c9f7dc8f42
tinbit25 <tinbitelias25@gmail.com>
2025-01-04 15:42:58 +0300

    ex-7

---
+++ 01_Day_JavaScript_Refresher/Exercise7/level1.js
@@ -0,0 +1,3 @@
+// Exercises Level 1
+// Create an Animal class. The class will have name, age, color, legs properties and create different methods
+// Create a Dog and Cat child class from the Animal Class.
