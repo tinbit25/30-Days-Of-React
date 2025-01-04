56ab91a32bdbd0e1530cc1aec3e9962e9ff541a0
tinbit25 <tinbitelias25@gmail.com>
2025-01-04 15:40:03 +0300

    finish ex-6

--- 01_Day_JavaScript_Refresher/Exercise6/level1.js
+++ 01_Day_JavaScript_Refresher/Exercise6/level1.js
@@ -41,3 +41,60 @@
 
 // Explain the difference between some and every
 
+const products = [
+      { product: 'banana', price: 3 },
+      { product: 'mango', price: 6 },
+      { product: 'potato', price: ' ' },
+      { product: 'avocado', price: 8 },
+      { product: 'coffee', price: 10 },
+      { product: 'tea', price: '' },
+    ]
+    //1
+    const price=products.forEach(product=>product.price?console.log(product.price):console.log('unknown'))
+    //2
+    products.forEach(product=>product.price?console.log(`The price of ${product.product} is ${product.price} euros.`):console.log(`The price of ${product.product} is unknown.`))
+    //3 
+    let totalPrice = 0;
+products.forEach(product => {
+    if (typeof product.price === 'number' && !isNaN(product.price)) {
+        totalPrice += product.price;
+    }
+});
+
+console.log("Total Price:", totalPrice); 
+    //4// Create an array of prices using map and store it in a variable prices
+   
+let prices = products.map(product => product.price);
+console.log("Prices:", prices);
+//5  Filter products with prices
+let prices1 = products.filter(product => product.price);
+console.log("Prices1:", prices1);
+//6// Use method chaining to get the sum of the prices(map, filter, reduce)
+let sum=products.filter(product => typeof product.price === 'number' && !isNaN(product.price)) 
+.map(product => product.price)
+.reduce((acc, price) => acc + price, 0); 
+
+console.log("Sum:", sum);
+//7 Calculate the sum of all the prices using reduce only
+let sum2 = products.reduce((acc, product) => {
+    if (typeof product.price === 'number' && !isNaN(product.price)) {
+        return acc + product.price;
+    }
+    return acc; 
+}, 0); 
+
+console.log("Sum2:", sum2); 
+//8 Find the first product which doesn't have a price value
+let product=products.find(product=>!product.price)
+console.log("Product:", product);
+//9 Find the index of the first product which does not have price value
+let index=products.findIndex(product=>!product.price)
+console.log("Index:", index);
+//10 Check if some products do not have a price value
+let some=products.some(product=>!product.price)
+console.log("Some:", some);
+//11 Check if all the products have price value
+let every=products.every(product=>product.price)
+console.log("Every:", every);
+
+
